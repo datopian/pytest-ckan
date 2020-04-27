a994e8cde4f5696e1489841533938defa6f61d0e
Shahar Evron <shahar.evron@datopian.com>
2020-04-27 16:06:14 +0300

    Update setup.py with some more metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
+from os import path
 from setuptools import setup
+
+
+# Get the long description from the relevant file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='pytest-ckan',
     packages=['pytest_ckan'],
     version=open('VERSION').read(),
     description='Backport of CKAN 2.9 pytest plugin and fixtures to CAKN 2.8',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Shahar Evron',
     author_email='shahar.evron@datopian.com',
+    copyright='Copyright 2020 (c) Viderum Inc. / Datopian',
+    license = 'MIT',
+    url='https://github.com/datopian/pytest-ckan',
     install_requires=[
         'pytest',
     ],
     entry_points={"pytest11": ["ckan = pytest_ckan"]},
-    classifiers=["Framework :: Pytest"],
+    keywords = ['ckan', 'pytest'],
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT',
+    ],
 )
